ddf219c2587fb8ffe2d08d43aa306b0b293fa12b
sabrinanefissa <sabrinanefissa@gmail.com>
2026-09-23 17:20:14 -0400

    Update phone photo for industry 3

--- content.js
+++ content.js
@@ -96,7 +96,7 @@ industry 2 photo: ind-retail.webp
 industry 2 phone photo: m/ind-retail.webp
 industry 3 name: Enterprise 
 industry 3 photo: assets/C2AB4C32-14F7-4A1F-BA37-BABA7A1356B5.jpeg
-industry 3 phone photo: m/ind-hospitality.webp
+industry 3 phone photo: assets/457AD153-F027-4D42-B1D6-7FD563ED9B66.png
 industry 4 name: Manufacturing
 industry 4 photo: mfg-on.webp
 industry 4 phone photo: m/mfg-on.webp
