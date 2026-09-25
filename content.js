7f90df5c2143f6132759767f0cf0988a3a9e9473
SNB <sabrinanefissa@gmail.com>
2026-09-25 00:25:45 -0400

    Adjust review speeds for various elements

--- content.js
+++ content.js
@@ -214,12 +214,12 @@ reviews link: https://www.g2.com/products/l-squared/reviews
 # SPEED. 100% is as it is now, 200% is twice as fast, 50% is half as fast.
 # The first line sets the whole section; the others set one moment each, on top of it.
 reviews speed: 100%
-reviews speed first review: 350%
-reviews speed gathering: 500%
+reviews speed first review: 200%
+reviews speed gathering: 300%
 reviews speed filling: 700%
-reviews speed zoom: 300%
+reviews speed zoom: 150%
 reviews speed mark hold: 150%
-reviews speed line slide: 1500%
+reviews speed line slide: 150%
 reviews speed line hold: 150%
 reviews speed line fade: 150%
 reviews speed logo hold: 150%
