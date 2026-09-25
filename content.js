51c4923fac27a9237427a13bc991751fb3e23a43
SNB <sabrinanefissa@gmail.com>
2026-09-25 00:19:01 -0400

    Increase reviews speed from 100% to 150%

--- content.js
+++ content.js
@@ -213,7 +213,7 @@ reviews button: Leave a review
 reviews link: https://www.g2.com/products/l-squared/reviews
 # SPEED. 100% is as it is now, 200% is twice as fast, 50% is half as fast.
 # The first line sets the whole section; the others set one moment each, on top of it.
-reviews speed: 100%
+reviews speed: 150%
 reviews speed first review: 200%
 reviews speed gathering: 200%
 reviews speed filling: 250%
